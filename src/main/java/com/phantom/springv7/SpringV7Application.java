d52ed1b0b7b99e9c3fb8b716c7df9802494302e1
phantomhunter <ASerFilyakov@mail.ru>
2022-06-15 00:08:12 +0300

    Initial commit

---
+++ src/main/java/com/phantom/springv7/SpringV7Application.java
@@ -0,0 +1,13 @@
+package com.phantom.springv7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringV7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringV7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/phantom/springv7/SpringV7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phantom.springv7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringV7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
