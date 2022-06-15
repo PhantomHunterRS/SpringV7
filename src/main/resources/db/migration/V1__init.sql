ad3296431f33df9d5dc2d99d97a83f802a54082f
phantomhunter <ASerFilyakov@mail.ru>
2022-06-15 23:57:56 +0300

    v1

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,128 @@
+CREATE TABLE IF NOT EXISTS SHOP.PRODUCT
+(
+    ID               BIGSERIAL      NOT NULL PRIMARY KEY,
+    TITLE            VARCHAR(255)   NOT NULL,
+    COST             DECIMAL(10, 2) NOT NULL
+    );
+
+insert into shop.PRODUCT (title, cost) values ('Apple - Northern Spy', 3483.35);
+insert into shop.PRODUCT (title, cost) values ('Cloves - Whole', 321.7);
+insert into shop.PRODUCT (title, cost) values ('Bacardi Breezer - Strawberry', 383.81);
+insert into shop.PRODUCT (title, cost) values ('Arizona - Plum Green Tea', 2334.05);
+insert into shop.PRODUCT (title, cost) values ('Bar Special K', 671.45);
+insert into shop.PRODUCT (title, cost) values ('Appetizer - Smoked Salmon / Dill', 3191.63);
+insert into shop.PRODUCT (title, cost) values ('Magnotta - Bel Paese White', 698.89);
+insert into shop.PRODUCT (title, cost) values ('Shrimp - 16/20, Iqf, Shell On', 778.2);
+insert into shop.PRODUCT (title, cost) values ('Hold Up Tool Storage Rack', 3637.77);
+insert into shop.PRODUCT (title, cost) values ('Wine - Cotes Du Rhone Parallele', 1469.61);
+insert into shop.PRODUCT (title, cost) values ('Cheese - Shred Cheddar / Mozza', 1155.33);
+insert into shop.PRODUCT (title, cost) values ('Oranges - Navel, 72', 2979.93);
+insert into shop.PRODUCT (title, cost) values ('Wine - Gato Negro Cabernet', 1220.27);
+insert into shop.PRODUCT (title, cost) values ('Cheese - Feta', 1913.18);
+insert into shop.PRODUCT (title, cost) values ('Oil - Olive', 3363.87);
+insert into shop.PRODUCT (title, cost) values ('Chicken Giblets', 4848.1);
+insert into shop.PRODUCT (title, cost) values ('Honey - Lavender', 484.47);
+insert into shop.PRODUCT (title, cost) values ('Tea - Mint', 1492.7);
+insert into shop.PRODUCT (title, cost) values ('Beef - Bones, Cut - Up', 3401.66);
+insert into shop.PRODUCT (title, cost) values ('Wine - Chateau Timberlay', 3052.49);
+insert into shop.PRODUCT (title, cost) values ('Longos - Burritos', 2740.6);
+insert into shop.PRODUCT (title, cost) values ('Wine - Red, Harrow Estates, Cab', 4104.96);
+insert into shop.PRODUCT (title, cost) values ('Food Colouring - Orange', 558.04);
+insert into shop.PRODUCT (title, cost) values ('The Pop Shoppe - Grape', 2160.05);
+insert into shop.PRODUCT (title, cost) values ('Chocolate - Mi - Amere Semi', 3865.96);
+insert into shop.PRODUCT (title, cost) values ('Pork Ham Prager', 1033.33);
+insert into shop.PRODUCT (title, cost) values ('Rum - Mount Gay Eclipes', 2733.37);
+insert into shop.PRODUCT (title, cost) values ('Parasol Pick Stir Stick', 2789.57);
+insert into shop.PRODUCT (title, cost) values ('Appetizer - Cheese Bites', 2794.64);
+insert into shop.PRODUCT (title, cost) values ('Mikes Hard Lemonade', 3307.95);
+insert into shop.PRODUCT (title, cost) values ('Cake - Cake Sheet Macaroon', 4044.06);
+insert into shop.PRODUCT (title, cost) values ('Wine - Riesling Alsace Ac 2001', 3137.46);
+insert into shop.PRODUCT (title, cost) values ('Juice - Orange, 341 Ml', 1750.15);
+insert into shop.PRODUCT (title, cost) values ('Coconut - Whole', 4699.15);
+insert into shop.PRODUCT (title, cost) values ('Lettuce - Curly Endive', 1033.53);
+insert into shop.PRODUCT (title, cost) values ('Cheese - Cottage Cheese', 3342.27);
+insert into shop.PRODUCT (title, costshop.) values ('Sauce Bbq Smokey', 4120.84);
+insert into shop.PRODUCT (title, cost) values ('Sherry - Dry', 4442.98);
+insert into shop.PRODUCT (title, cost) values ('Pepper - Green Thai', 1274.74);
+insert into shop.PRODUCT (title, cost) values ('Cheese - Gouda', 4510.97);
+insert into shop.PRODUCT (title, cost) values ('Pepper - Chillies, Crushed', 37.72);
+insert into shop.PRODUCT (title, cost) values ('Smoked Paprika', 2356.56);
+insert into shop.PRODUCT (title, cost) values ('Cheese - Havarti, Salsa', 2729.550);
+insert into shop.PRODUCT (title, cost) values ('Tandoori Curry Paste', 1855.71);
+insert into shop.PRODUCT (title, cost) values ('Wine - Zonnebloem Pinotage', 4958.47);
+insert into shop.PRODUCT (title, cost) values ('Wine - Chenin Blanc K.w.v.', 250.47);
+insert into shop.PRODUCT (title, cost) values ('Rolled Oats', 4320.48);
+insert into shop.PRODUCT (title, cost) values ('Cheese - Roquefort Pappillon', 1941.85);
+insert into shop.PRODUCT (title, cost) values ('Cotton Wet Mop 16 Oz', 3332.02);
+insert into shop.PRODUCT (title, cost) values ('Salt - Rock, Course', 1024.9);
+insert into shop.PRODUCT (title, cost) values ('Chick Peas - Dried', 3981.23);
+insert into shop.PRODUCT (title, cost) values ('Stock - Veal, Brown', 3388.57);
+insert into shop.PRODUCT (title, cost) values ('Coriander - Ground', 1030.47);
+insert into shop.PRODUCT (title, cost) values ('Pepsi - 600ml', 4858.68);
+insert into shop.PRODUCT (title, cost) values ('Tart Shells - Savory, 3', 87.95);
+insert into shop.PRODUCT (title, cost) values ('Bagels Poppyseed', 3021.54);
+insert into shop.PRODUCT (title, cost) values ('Dill Weed - Fresh', 1670.77);
+insert into shop.PRODUCT (title, cost) values ('Wine - Jaboulet Cotes Du Rhone', 1852.13);
+insert into shop.PRODUCT (title, cost) values ('Muffin Mix - Oatmeal', 4733.9);
+insert into shop.PRODUCT (title, cost) values ('Potatoes - Yukon Gold, 80 Ct', 2635.93);
+insert into shop.PRODUCT (title, cost) values ('Chutney Sauce', 3998.22);
+insert into shop.PRODUCT (title, cost) values ('Ecolab Digiclean Mild Fm', 4564.81);
+insert into shop.PRODUCT (title, cost) values ('Southern Comfort', 1140.86);
+insert into shop.PRODUCT (title, cost) values ('Crab - Claws, Snow 16 - 24', 808.96);
+insert into shop.PRODUCT (title, cost) values ('Sprouts - Brussel', 4023.41);
+insert into shop.PRODUCT (title, cost) values ('Butter - Pod', 3902.46);
+insert into shop.PRODUCT (title, cost) values ('Foam Espresso Cup Plain White', 1189.92);
+insert into shop.PRODUCT (title, cost) values ('Cheese - Bocconcini', 4282.9);
+insert into shop.PRODUCT (title, cost) values ('Noodles - Cellophane, Thin', 3470.29);
+insert into shop.PRODUCT (title, cost) values ('Sugar - Brown', 483.25);
+insert into shop.PRODUCT (title, cost) values ('Vodka - Hot, Lnferno', 1391.58);
+insert into shop.PRODUCT (title, cost) values ('Apple - Royal Gala', 523.48);
+insert into shop.PRODUCT (title, cost) values ('Schnappes - Peach, Walkers', 4041.0);
+insert into shop.PRODUCT (title, cost) values ('Jagermeister', 1005.55);
+insert into shop.PRODUCT (title, cost) values ('Miso Paste White', 4010.37);
+insert into shop.PRODUCT (title, cost) values ('Chips - Assorted', 3524.57);
+insert into shop.PRODUCT (title, cost) values ('Pepper - Red Bell', 156.65);
+insert into shop.PRODUCT (title, cost) values ('Wine - Wyndham Estate Bin 777', 227.48);
+insert into shop.PRODUCT (title, cost) values ('Sprouts - China Rose', 1286.63);
+insert into shop.PRODUCT (title, cost) values ('Cheese - Augre Des Champs', 4657.72);
+insert into shop.PRODUCT (title, cost) values ('C - Plus, Orange', 858.83);
+insert into shop.PRODUCT (title, cost) values ('Wine - Redchard Merritt', 3781.1);
+insert into shop.PRODUCT (title, cost) values ('Potatoes - Instant, Mashed', 3387.54);
+insert into shop.PRODUCT (title, cost) values ('Beer - Corona', 3168.75);
+insert into shop.PRODUCT (title, cost) values ('Steampan Lid', 934.94);
+insert into shop.PRODUCT (title, cost) values ('Cardamon Seed / Pod', 4463.39);
+insert into shop.PRODUCT (title, cost) values ('Extract - Raspberry', 2309.59);
+insert into shop.PRODUCT (title, cost) values ('Savory', 4964.36);
+insert into shop.PRODUCT (title, cost) values ('Fireball Whisky', 4986.94);
+insert into shop.PRODUCT (title, cost) values ('Dates', 2766.19);
+insert into shop.PRODUCT (title, cost) values ('French Pastries', 2343.03);
+insert into shop.PRODUCT (title, cost) values ('Chocolate - Milk, Callets', 3201.35);
+insert into shop.PRODUCT (title, cost) values ('Strawberries - California', 603.99);
+insert into shop.PRODUCT (title, cost) values ('Bread - Multigrain Oval', 3221.73);
+insert into shop.PRODUCT (title, cost) values ('Flavouring Vanilla Artificial', 3946.47);
+insert into shop.PRODUCT (title, cost) values ('Water - Spring Water, 355 Ml', 700.4);
+insert into shop.PRODUCT (title, cost) values ('Red Currants', 3170.31);
+insert into shop.PRODUCT (title, cost) values ('Soup - Base Broth Chix', 3869.8);
+insert into shop.PRODUCT (title, cost) values ('Glass - Juice Clear 5oz 55005', 155.34);
+
+
+CREATE TABLE IF NOT EXISTS SHOP.BUYER
+(
+    ID               BIGSERIAL      NOT NULL PRIMARY KEY,
+    NAME             VARCHAR(255)   NOT NULL
+    );
+
+insert into shop.buyer(name) values ('Martin');
+insert into shop.buyer(name) values ('Stuart');
+insert into shop.buyer(name) values ('Rikki');
+insert into shop.buyer(name) values ('Trash');
+insert into shop.buyer(name) values ('Jimmy');
+insert into shop.buyer(name) values ('Steven');
+insert into shop.buyer(name) values ('Mammy');
+
+CREATE TABLE IF NOT EXISTS SHOP.BUYER_PRODUCTS(
+                                                  BUYER_ID        BIGINT,
+                                                  PRODUCT_ID     BIGINT,
+                                                  FOREIGN KEY (BUYER_ID) REFERENCES SHOP.BUYER(ID),
+    FOREIGN KEY (PRODUCT_ID) REFERENCES SHOP.PRODUCT(ID)
+    );
