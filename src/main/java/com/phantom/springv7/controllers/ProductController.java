b2008f45b628ae40ef5c3ec0ded0916e5da4db9d
phantomhunter <ASerFilyakov@mail.ru>
2022-06-27 00:02:21 +0300

    v1

--- src/main/java/com/phantom/springv7/controllers/ProductController.java
+++ src/main/java/com/phantom/springv7/controllers/ProductController.java
@@ -1,11 +1,15 @@
 package com.phantom.springv7.controllers;
 
+import com.phantom.springv7.exceptions.MarketError;
 import com.phantom.springv7.model.Product;
 import com.phantom.springv7.services.ProductService;
 import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -18,10 +22,23 @@ public class ProductController {
     public List<Product> findAll(){
         return productService.findAll();
     }
+//    @GetMapping("/product/{id}")
+//    public ResponseEntity<?> findById(@PathVariable Long id){
+//        Optional<Product> product = productService.findById(id);
+//        if (product.isEmpty()){
+//           return new ResponseEntity<>(new MarketError("Product id = "+ id + "not found"),HttpStatus.NOT_FOUND);
+//        }
+//        return new ResponseEntity<>(product, HttpStatus.OK);
+//    }
+
+// V2 exception
     @GetMapping("/product/{id}")
     public Product findById(@PathVariable Long id){
+
+
         return productService.findById(id).get();
     }
+
     @PostMapping("/newProduct")
     public Product create(@RequestBody Product product){
         Product productIn = new Product();
